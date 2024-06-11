cab12a4e11a8366165f2f1f6a096995547c9eef6
Michael Wornow <mwornow98@gmail.com>
2024-06-11 13:52:30 -0700

    Update run_experiments.py

--- wonderbread/benchmark/tasks/improvement/sop_ranking/run_experiments.py
+++ wonderbread/benchmark/tasks/improvement/sop_ranking/run_experiments.py
@@ -4,10 +4,11 @@ from wonderbread.benchmark.tasks.improvement.sop_ranking.main import run
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument( "--model", type=str, default="GPT4", choices=["GPT4", "GeminiPro", "Claude3"] )
+parser.add_argument("--is_debug", action='store_true', default=False, help="If set, run in debug mode (only 3 examples)")
 args = parser.parse_args()
 
 kwarg_settings = [
     {},
 ]
 
-run_experiment(run, __file__, kwarg_settings, n_threads=1, model=args.model, is_path_to_demo_folder=False, is_use_rank_1_df=True, is_skip_completed_ablations=True)
+run_experiment(run, __file__, kwarg_settings, n_threads=1, model=args.model, is_debug=args.is_debug, is_path_to_demo_folder=False, is_use_rank_1_df=True, is_skip_completed_ablations=True)
