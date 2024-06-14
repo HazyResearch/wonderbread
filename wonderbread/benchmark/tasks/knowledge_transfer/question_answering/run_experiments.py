4b7167a6c01221224def4252dc9a81335c903e1e
Michael Wornow <mwornow98@gmail.com>
2024-06-14 00:00:35 -0700

    Update run_experiments.py

--- wonderbread/benchmark/tasks/knowledge_transfer/question_answering/run_experiments.py
+++ wonderbread/benchmark/tasks/knowledge_transfer/question_answering/run_experiments.py
@@ -10,12 +10,14 @@ args = parser.parse_args()
 path_to_demos: str = get_rel_path(__file__, "../../../../../data/demos")
 path_to_qa: str = get_rel_path(__file__, "../../../../../data/qa_dataset.csv")
 path_to_script_dir: str = os.path.dirname(__file__)
+path_to_output_dir: str = os.path.join(path_to_script_dir, 'outputs/')
 
-os.system(f"python {os.path.join(path_to_script_dir, 'generate_responses.py')} {path_to_demos} --path_to_input_csv {path_to_qa} --model {args.model} {'--is_debug' if args.is_debug else ''}")
+os.system(f"python {os.path.join(path_to_script_dir, 'generate_responses.py')} {path_to_demos} --path_to_input_csv {path_to_qa} --path_to_output_dir {path_to_output_dir} --model {args.model} {'--is_debug' if args.is_debug else ''}")
 
-if not os.path.exists(f"./outputs/qna_{args.model}.csv"):
-    print(f"ERROR: No output file generated from `generate_responses.py`. Expected file at: `./outputs/qna_{args.model}.csv`")
+path_to_output_file: str = os.path.join(path_to_output_dir, f"qna_{args.model}.csv")
+if not os.path.exists(path_to_output_file):
+    print(f"ERROR: No output file generated from `generate_responses.py`. Expected file at: `{path_to_output_file}`")
     exit(1)
     
-os.system(f'python {os.path.join(path_to_script_dir, "eval.py")} "./outputs/qna_{args.model}.csv"')
+os.system(f'python {os.path.join(path_to_script_dir, "eval.py")} "{path_to_output_file}"')
 os.system(f"python {os.path.join(path_to_script_dir, 'collect_results.py')} ./outputs/")
